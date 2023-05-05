e0114cff16d4313470265392dfb12d0717b199cc
abdulalikhan <k190128@nu.edu.pk>
2023-05-05 11:07:57 +0500

    AB#12: Test the Add Product Route

---
+++ tests/test_create.py
@@ -0,0 +1,41 @@
+from app import app as flask_app
+import pytest
+import json
+
+
+@pytest.fixture
+def client():
+    with flask_app.test_client() as client:
+        yield client
+
+
+def test_create_product(client):
+    data = {
+        'name': 'Test Product',
+        'description': 'This is a test product',
+        'price': 19.99,
+        'quantity': 10
+    }
+
+    response = client.post('/products', json=data)
+
+    assert response.status_code == 200
+
+    expected_message = 'Product created successfully'
+    new_id = 0
+    with open('products.json', 'r') as f:
+        products = json.load(f)
+        new_id = len(products)
+    expected_data = {
+        'id': new_id,
+        'name': 'Test Product',
+        'description': 'This is a test product',
+        'price': 19.99,
+        'quantity': 10
+    }
+    assert response.json == {
+        'message': expected_message, 'data': expected_data}
+
+    response = client.delete(f'/products/{new_id}')
+    assert response.status_code == 200
+    assert response.json['message'] == 'Product deleted successfully'
