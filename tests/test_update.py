c36137c6d91f55be4720db6d1e04aff362d74799
abdulalikhan <k190128@nu.edu.pk>
2023-05-05 11:08:15 +0500

    AB#14: Test the Update Product Quantity Route

---
+++ tests/test_update.py
@@ -0,0 +1,33 @@
+import json
+import pytest
+from app import app as flask_app
+
+
+@pytest.fixture
+def client():
+    with flask_app.test_client() as client:
+        yield client
+
+
+def test_update_product(client):
+    new_product_data = {'name': 'New Product',
+                        'description': 'A new product', 'price': 9.99, 'quantity': 10}
+    response = client.post('/products', json=new_product_data)
+    assert response.status_code == 200
+
+    new_product_id = response.json['data']['id']
+
+    update_data = {'quantity': 5}
+    response = client.patch(f'/products/{new_product_id}', json=update_data)
+    assert response.status_code == 200
+    assert response.json['message'] == 'Product updated successfully'
+
+    response = client.get(f'/products')
+    assert response.status_code == 200
+
+    updated_product_data = response.json[-1]
+    assert updated_product_data['quantity'] == 5
+
+    response = client.delete(f'/products/{new_product_id}')
+    assert response.status_code == 200
+    assert response.json['message'] == 'Product deleted successfully'
