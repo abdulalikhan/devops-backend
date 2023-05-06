17aaae66a668be5bb811be0d72b9d995e2052c5b
abdulalikhan <k190128@nu.edu.pk>
2023-05-06 22:56:21 +0500

    Modified Automation Tests

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -10,7 +10,7 @@ def test_products_frontend():
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
 
-    driver.get("http://devops-frontend-staging.azurewebsites.net/")
+    driver.get("https://devops-frontend-staging.azurewebsites.net")
 
     product_table = driver.find_element(
         By.CLASS_NAME, "product-table-container")
@@ -26,27 +26,17 @@ def test_products_frontend():
     desc_input.send_keys("A new product")
     quantity_input = add_product_form.find_element(By.ID, "quantity")
     quantity_input.send_keys("5")
-    add_button = add_product_form.find_element(By.TAG_NAME, "button")
-    add_button.click()
+    price_input = add_product_form.find_element(By.ID, "price")
+    price_input.send_keys("1.8")
+    add_button = add_product_form.find_element(By.ID, "add")
+    add_button.submit()
 
-    next_button = driver.find_element(By.CLASS_NAME, "next-button")
-    while True:
-        disabled_attr = next_button.get_attribute("disabled")
-        if disabled_attr is None or disabled_attr == "disabled":
-            break
-        next_button.click()
-        time.sleep(1)
+    time.sleep(5)
 
+    product_table = driver.find_element(
+        By.CLASS_NAME, "product-table-container")
     new_product_row = product_table.find_element(
-        By.XPATH, "//*[@id='app']/div/table/tbody/tr[4]/td[1]")
+        By.XPATH, "/html/body/div/table/tbody/tr[13]")
     assert new_product_row is not None
-
-    del_button = driver.find_element(
-        By.XPATH, "//*[@id='app']/div/table/tbody/tr[4]/td[5]/button")
-    del_button.click()
-
-    with pytest.raises(Exception):
-        product_table.find_element(
-            By.XPATH, "//*[@id='app']/div/table/tbody/tr[6]/td[1]")
 
     driver.quit()
